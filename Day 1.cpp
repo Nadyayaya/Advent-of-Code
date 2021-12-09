58eab2b166345f0598b24d52442803d2dcc3fd6b
Nadyayaya <95830951+Nadyayaya@users.noreply.github.com>
2021-12-09 13:13:37 +1300

    Update and rename Day 1 to Day 1.cpp

---
+++ Day 1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+	vector<string> v;
+	ifstream file("day1.txt");
+	if(file.is_open()) {
+		string line;
+		while(getline(file, line)) {
+			v.push_back(line);
+		}
+	}
+	file.close();
+	int numincreased = 1;
+	for(int i = 1; i < v.size(); i++) {
+		// check item before
+		if(v[i] > v[i - 1]) {
+			numincreased++;
+		}
+	}
+	std::cout << numincreased << std::endl;
+	return 0;
+}
